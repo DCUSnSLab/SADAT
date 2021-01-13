5fe23e0cad4a2a1e82a02a6470f140fe555f6116
박준홍 <junhp12345@gmail.com>
2021-01-14 02:44:01 +0900

    Init tracker
    Former-commit-id: 8b5d0c26dc8041232d198b53b18d4525d2ca2044

--- SADAT/externalmodules/default/tracker.py
+++ SADAT/externalmodules/default/tracker.py
@@ -1,13 +1,32 @@
 from dadatype.dtype_tracker import dtype_tracker
 from externalmodules.default.dataset_enum import senarioBasicDataset
 from externalmodules.extModule import extModule
+import mytracker # Project Structure에서 Tracker Project를 추가하여야 함
+
+from random import *
+
+from sensor.SenAdptMgr import AttachedSensorName
 
 
 class trackerBasic(extModule):
     def __init__(self):
         super().__init__('trackerBasic')
+        # self.track_xy = mytracker.dbscanClustering(60, 2) # eps, min_samples
+        self.idx = 0
+        #
+
+    def getRawDatabyKey(self, key):
+        return self._getRawDatabyKey('rplidar')
 
     def do(self):
-        tdata = dtype_tracker(0, 300, 10, 20, 50)
+        # tdata = dtype_tracker(0, 300, 10, 20, 50)
+
+        # x, y = mytracker.dbscanClustering()
+
+        mytracker.aa(self)
+        tdata = dtype_tracker(0, 10, 10, 50, 50)
+        # tdata = dtype_tracker(0, randrange(200), randrange(200), 50, 50)
+        # tdata = dtype_tracker(0, self.track_xy[self.idx][0], self.track_xy[self.idx][1], 50, 50)
         self._addData(datakey=senarioBasicDataset.TRACK, data=tdata)
+        self.idx = self.idx + 1
         #print('do ExtModules -->', self.getName())
