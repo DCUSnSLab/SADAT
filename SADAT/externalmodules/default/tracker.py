79f6339ca6f203266f74bce60d03ccdec1e26079
박준홍 <junhp12345@gmail.com>
2021-01-15 01:17:19 +0900

    Modify tracker.py 	- Tracking bbox movement via Track project library
    Former-commit-id: 88fd8c1468c00e8f78d8df47709b3f14a9696299

--- SADAT/externalmodules/default/tracker.py
+++ SADAT/externalmodules/default/tracker.py
@@ -7,26 +7,23 @@ from random import *
 
 from sensor.SenAdptMgr import AttachedSensorName
 
-
 class trackerBasic(extModule):
     def __init__(self):
         super().__init__('trackerBasic')
         # self.track_xy = mytracker.dbscanClustering(60, 2) # eps, min_samples
-        self.idx = 0
-        #
 
     def getRawDatabyKey(self, key):
-        return self._getRawDatabyKey('rplidar')
+        return self._getRawDatabyKey(key)
+
+    def getLidarDatabyKey(self, key):
+        return self._getData()
 
     def do(self):
         # tdata = dtype_tracker(0, 300, 10, 20, 50)
 
-        # x, y = mytracker.dbscanClustering()
-
-        mytracker.aa(self)
-        tdata = dtype_tracker(0, 10, 10, 50, 50)
+        tracker_x, tracker_y = mytracker.getlidardata(self)
+        # tdata = dtype_tracker(0, 10, 10, 50, 50)
         # tdata = dtype_tracker(0, randrange(200), randrange(200), 50, 50)
-        # tdata = dtype_tracker(0, self.track_xy[self.idx][0], self.track_xy[self.idx][1], 50, 50)
+        tdata = dtype_tracker(0, tracker_x, tracker_y, 50, 50)
         self._addData(datakey=senarioBasicDataset.TRACK, data=tdata)
-        self.idx = self.idx + 1
         #print('do ExtModules -->', self.getName())
