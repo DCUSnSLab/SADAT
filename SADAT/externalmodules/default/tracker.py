d79bc920f766cb4edc6f68562847cf06667f2ce9
박준홍 <junhp12345@gmail.com>
2021-01-18 22:11:33 +0900

    Add bbox for all cluster object
    Former-commit-id: 04affa38f30c4590aa089755caf77df5f3528b50

--- SADAT/externalmodules/default/tracker.py
+++ SADAT/externalmodules/default/tracker.py
@@ -19,11 +19,15 @@ class trackerBasic(extModule):
         return self._getData()
 
     def do(self):
-        tracker_x, tracker_y, height, width = mytracker.getlidardata(self)
-        tdata = dtype_tracker(0, tracker_x, tracker_y, 50, 50)
+        # tracker_x, tracker_y, height, width = mytracker.getlidardata(self)
+        tracker_list = mytracker.getlidardata(self)
+        for tracker_x, tracker_y in tracker_list:
+            tdata = dtype_tracker(0, tracker_x, tracker_y, 50, 50)
+            self._addData(datakey=senarioBasicDataset.TRACK, data=tdata)
         # TODO mytracker.getlidardata의 리턴값을 tracker_x, tracker_y, (x, y), (x, y) 로 수정하여야 함.
         # mytracker 코드에서 height, width 값을 가져오도록 되어있는데,
         # 제대로 된 Tracker 구현 이후 생성된 Tracker의 좌하단, 우상단 좌표값을 가져오는 코드로
         # 수정 진행하여야 함.
-        self._addData(datakey=senarioBasicDataset.TRACK, data=tdata)
+        # tdata = dtype_tracker(0, 10, 10, 50, 50)
+        # self._addData(datakey=senarioBasicDataset.TRACK, data=tdata)
         #print('do ExtModules -->', self.getName())
