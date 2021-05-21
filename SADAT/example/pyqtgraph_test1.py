d31d82c2dcc337988d30c2d8f2f711ac77448795
marsberry <marsberry@cu.ac.kr>
2021-05-22 01:32:33 +0900

    modified
    Former-commit-id: b289cd2f41cca29e049f415a10684b3c0309d43b

--- SADAT/example/pyqtgraph_test1.py
+++ SADAT/example/pyqtgraph_test1.py
@@ -121,35 +121,16 @@ fig1.setBackgroundColor(background_color)
 #     #plt = gl.GLScatterPlotItem(pos=pts, color=pg.glColor((i, n * 1.3)), size=5, pxMode=True)
 #     fig1.addItem(plt)
 
-##
-##  First example is a set of points with pxMode=False
-##  These demonstrate the ability to have points with real size down to a very small scale
-##
-pos = np.empty((53, 3))
-size = np.empty((53))
-color = np.empty((53, 4))
-pos[0] = (1, 0, 0);
-size[0] = 0.5;
-color[0] = (1.0, 0.0, 0.0, 0.5)
-pos[1] = (0, 1, 0);
-size[1] = 0.2;
-color[1] = (0.0, 0.0, 1.0, 0.5)
-pos[2] = (0, 0, 1);
-size[2] = 2. / 3.;
-color[2] = (0.0, 1.0, 0.0, 0.5)
-
-z = 0.5
-d = 6.0
-for i in range(3, 53):
-    pos[i] = (0, 0, z)
-    size[i] = 2. / d
-    color[i] = (0.0, 1.0, 0.0, 0.5)
-    z *= 0.5
-    d *= 2.0
-
-sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
-sp1.translate(5, 5, 0)
-fig1.addItem(sp1)
+pos = np.random.random(size=(512 * 256, 3))
+pos *= [10, -10, 10]
+d2 = (pos ** 2).sum(axis=1) ** 0.5
+pos[:, 2] = d2
+color = [1, 0, 0, 0.5]
+size = 5
+print(pos)
+sp = gl.GLScatterPlotItem(pos=pos, color = pg.glColor((0,3)), size=5, pxMode=False)
+sp.translate(-127.5, -127.5, -127.5)
+fig1.addItem(sp)
 
 # axis = Custom3DAxis(fig1, color=(0.2,0.2,0.2,.6))
 # axis.setSize(x=12, y=12, z=12)
