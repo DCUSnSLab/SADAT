cfb51d1ed991742fc8ca8abdd952e27dc108a808
soobinJeon-Linux <marsberry@cu.ac.kr>
2021-05-31 20:04:14 +0900

    adeed pointcloud2 example
    Former-commit-id: 61ac5328a9b56fd56f3d884fddb380fc3bb6b840

--- SADAT/example/pointcloud2.py
+++ SADAT/example/pointcloud2.py
@@ -1,2 +1,21 @@
+import rospy
+from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
-from sensor_msgs.msg import LaserScan
+import ros_numpy
+import numpy as np
+
+def callback(data):
+    field_names = [f.name for f in data.fields]
+    print(field_names)
+    pc = ros_numpy.numpify(data)
+    points=np.zeros((pc.shape[0],3))
+    points[:,0]=pc['x']
+    points[:,1]=pc['y']
+    points[:,2]=pc['z']
+    print(pc[0])
+    print(points[0])
+    #p = pcl.PointCloud(np.array(points, dtype=np.float32))
+
+rospy.init_node('listener', anonymous=True)
+rospy.Subscriber("/velodyne_points", PointCloud2, callback)
+rospy.spin()
