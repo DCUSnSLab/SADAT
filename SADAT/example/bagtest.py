f45147495a420b7da627f483e47521c09df1d726
yuri <kyuri0215@cu.ac.kr>
2022-01-17 16:16:30 +0900

    jira git 연동

--- SADAT/example/bagtest.py
+++ SADAT/example/bagtest.py
@@ -1,5 +1,6 @@
 import rosbag
 bag = rosbag.Bag('/home/soobin/development/dataset/2021_07_22_outdoor_centerpark.bag')
+
 rmsg = bag.read_messages()
 for topic, msg, t in rmsg:
     print(topic, t, msg)
